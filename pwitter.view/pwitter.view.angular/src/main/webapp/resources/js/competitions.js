5cd31367837e283f08443c5fdf609821c92e52bf
Huguet <huguet10@gmail.com>
2015-05-06 21:15:31 +0200

    Se muestra el listado, se borran y se muestra detalle

--- pwitter.view/pwitter.view.angular/src/main/webapp/resources/js/competitions.js
+++ pwitter.view/pwitter.view.angular/src/main/webapp/resources/js/competitions.js
@@ -1,7 +1,34 @@
-var app = angular.module('myApp', []);
-app.controller('customersCtrl', function($scope, $http) {
+var app = angular.module('competitionsApp', []);
+app.controller('competitionsController', function($scope, $http) {
 	$http.get("http://localhost:8080/api/competition").success(
 			function(response) {
 				$scope.competitions = response;
+
+				$scope.add = function(data) {
+					debugger;
+				};
+				$scope.edit = function(data) {
+					debugger;
+				};
+				$scope.erase = function(data) {
+					$http.delete(
+							"http://localhost:8080/api/competition/" + data.id)
+							.success(function(response) {
+								$http.get("http://localhost:8080/api/competition").success(function(response){
+									$scope.competitions = response;
+								});
+								$scope.competition=undefined;
+							});
+				};
+				$scope.detail = function(data) {
+					$http.get(
+							"http://localhost:8080/api/competition/" + data.id)
+							.success(function(response) {
+								$scope.competition=response;
+								$scope.closeDetail=function(){
+									$scope.competition=undefined;
+								};
+							});
+				};
 			});
 });
