eaae5d465c347252f64e70ebffc1aadc69ec8cb7
dmRoberto <roberto.d20@gmail.com>
2015-05-24 12:25:12 +0200

    Se sustituye la carga de contexto de spring por la de jsf

--- pwitter.view/pwitter.view.pfaces/src/main/java/es/upm/miw/pwitter/view/pfaces/config/PFacesConfig.java
+++
@@ -1,9 +0,0 @@
-package es.upm.miw.pwitter.view.pfaces.config;
-
-
-//@Configuration
-//@Import(RestConfig.class)
-//@ComponentScan("es.upm.miw.pwitter.view.pfaces")
-public class PFacesConfig {
-
-}
