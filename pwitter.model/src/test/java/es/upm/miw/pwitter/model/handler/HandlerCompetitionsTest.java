3972f140e95b5fbecfbbe13fd29e56fa2630f9f4
dmRoberto <roberto.d20@gmail.com>
2015-06-08 16:57:00 +0200

    Solucion bug menor en ejecucion de test en el modelo

--- pwitter.model/src/test/java/es/upm/miw/pwitter/model/handler/HandlerCompetitionsTest.java
+++ pwitter.model/src/test/java/es/upm/miw/pwitter/model/handler/HandlerCompetitionsTest.java
@@ -5,7 +5,10 @@ import static org.junit.Assert.assertEquals;
 import javax.inject.Inject;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 public class HandlerCompetitionsTest extends AbstractTest {
 
 	@Inject
